6b7df2a3fa21970de1798b8348bd376389612c01
info@qweb-solutions.co.za <thekos@infoverge.co.za>
2021-03-09 14:17:40 +0200

    View Binding

--- app/src/main/java/com/example/myapplication/ui/DetailFlragment.java
+++ app/src/main/java/com/example/myapplication/ui/DetailFlragment.java
@@ -4,27 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.myapplication.R;
+import com.example.myapplication.databinding.FragmentDetailFlragmentBinding;
 import com.example.myapplication.model.Hero;
 
 public class DetailFlragment extends Fragment {
 
-
-    private Hero hero;
+private FragmentDetailFlragmentBinding binding;
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_flragment, container, false);
+    public View onCreateView (LayoutInflater inflater,
+                              ViewGroup container,
+                              Bundle savedInstanceState) {
+        binding = FragmentDetailFlragmentBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+
+        return view;
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -33,14 +36,19 @@ public class DetailFlragment extends Fragment {
         if(getArguments() != null){
             DetailFlragmentArgs args = DetailFlragmentArgs.fromBundle(getArguments());
 
-            hero = args.getHero();
+            Hero hero = args.getHero();
+            onDisplay(hero);
         }
     }
-
+void onDisplay(Hero hero){
+        binding.txtId.setText(String.valueOf(hero.getId()));
+        binding.txtUserTitle.setText(String.valueOf(hero.getTitle()));
+        binding.txtUserBody.setText(String.valueOf(hero.getBody()));
+}
     @Override
     public void onResume() {
         super.onResume();
 
-        Toast.makeText(getActivity(), hero.getTitle() + " \n" + "Test", Toast.LENGTH_LONG).show();
+       // Toast.makeText(getActivity(), hero.getTitle() + " \n" + "Test", Toast.LENGTH_LONG).show();
     }
 }
